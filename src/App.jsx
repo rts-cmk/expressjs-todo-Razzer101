f53434052c0aeb78cac179bc49aa6ca31397388c
Razzer101 <Rasmus@anders-net.dk>
2025-11-26 13:42:01 +0100

    feat: Added todo POST

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [todoData, setTodoData] = useState(null)
+
+  const submitHandler = event => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+
+    const data = Object.fromEntries(formData)
+
+    fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+  }
+
+  useEffect(() => {
+    
+    fetch("http://localhost:3000/todos")
+    .then(response => response.json())
+    .then(data => setTodoData(data))
+
+  },[])
 
   return (
     <>
-      <form>
+      <h2>Post in todo:</h2>
+      <form onSubmit={submitHandler}>
         <label htmlFor="title">
           <span>Skriv todo titel: </span>
           <input type="text" name="title" id="title" />
@@ -21,6 +50,19 @@ function App() {
         <br />
         <button type="submit">Post</button>
       </form>
+      <section>
+        {todoData !== null &&
+          todoData.map((elm) => {
+            return(
+              <article key={elm.title}>
+                <h2>{elm.title}</h2>
+                <p>{elm.description}</p>
+                <p>Status: {elm?.done ? "færdig" : "ikke færdig"}</p>
+              </article>
+            )
+          })
+        }
+      </section>
     </>
   )
 }
