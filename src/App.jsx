0638b5d2f8120ad3f9fe34c8ea90a3d826503adb
Razzer101 <Rasmus@anders-net.dk>
2025-11-27 08:45:05 +0100

    chore: Installed React-Router

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import { useEffect, useState } from "react"
 function App() {
 
   const [todoData, setTodoData] = useState(null)
+  const [findTodoData, setFindToDoData] = useState(null)
+  const [update, setUpdate] = useState(true)
 
-  const submitHandler = event => {
+  useEffect(() => {
+    
+    fetch("http://localhost:3000/todos")
+    .then(response => response.json())
+    .then(data => setTodoData(data))
+
+  },[update])
+
+  const POSTHandler = event => {
     event.preventDefault()
 
     const formData = new FormData(event.target)
@@ -17,21 +27,28 @@ function App() {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(data)
-    })
+    }).then(() => {update ? setUpdate(false) : setUpdate(true)})
   }
 
-  useEffect(() => {
-    
-    fetch("http://localhost:3000/todos")
-    .then(response => response.json())
-    .then(data => setTodoData(data))
+  const findIdHandler = event => {
+    event.preventDefault()
 
-  },[])
+    const formData = new FormData(event.target)
+
+    const data = Object.fromEntries(formData)
+
+    const todoId = data.id - 1
+
+    fetch(`http://localhost:3000/todos/${todoId}`)
+    .then(response => response.json())
+    .then(data => setFindToDoData(data))
+
+  }
 
   return (
     <>
       <h2>Post in todo:</h2>
-      <form onSubmit={submitHandler}>
+      <form onSubmit={POSTHandler}>
         <label htmlFor="title">
           <span>Skriv todo titel: </span>
           <input type="text" name="title" id="title" />
@@ -52,10 +69,10 @@ function App() {
       </form>
       <section>
         {todoData !== null &&
-          todoData.map((elm) => {
+          todoData.map((elm, index) => {
             return(
               <article key={elm.title}>
-                <h2>{elm.title}</h2>
+                <h2>{index + 1}: {elm.title}</h2>
                 <p>{elm.description}</p>
                 <p>Status: {elm?.done ? "færdig" : "ikke færdig"}</p>
               </article>
@@ -63,6 +80,22 @@ function App() {
           })
         }
       </section>
+      <form onSubmit={findIdHandler}>
+        <label htmlFor="id">
+          <span>Indtast todo nr: </span>
+          <input type="number" name="id" id="id" />
+        </label>
+        <br />
+        <button type="submit">Find</button>
+      </form>
+      {
+        findTodoData !== null &&
+        <article>
+          <h2>{findTodoData.title}</h2>
+          <p>{findTodoData.description}</p>
+          <p>Status: {findTodoData?.done ? "færdig" : "ikke færdig"}</p>
+        </article>
+      }
     </>
   )
 }
